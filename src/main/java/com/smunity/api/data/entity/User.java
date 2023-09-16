36727f6dac4e3c1dc69ebe1cca2a274d5ccadf88
hyunmin0317 <choihm9903@naver.com>
2023-09-16 17:41:36 +0900

    feat: User 엔티티 (#9)

---
+++ src/main/java/com/smunity/api/data/entity/User.java
@@ -0,0 +1,99 @@
+package com.smunity.api.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "auth_user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private LocalDateTime last_login;
+
+    @Column(nullable = false)
+    private boolean is_superuser;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String first_name;
+
+    @Column(nullable = false)
+    private String last_name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private boolean is_staff;
+
+    @Column(nullable = false)
+    private boolean is_active;
+
+    @Column
+    @CreatedDate
+    private LocalDateTime date_joined;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.is_active;
+    }
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.is_active;
+    }
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Override
+    public boolean isEnabled() {
+        return this.is_active;
+    }
+}
