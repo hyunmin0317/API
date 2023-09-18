c879440ea525be4a204341360a7076453871707d
hyunmin0317 <choihm9903@naver.com>
2023-09-18 18:03:35 +0900

    feat: service 회원가입 메서드 작성 (#6)

--- src/main/java/com/smunity/api/service/impl/AccountServiceImpl.java
+++ src/main/java/com/smunity/api/service/impl/AccountServiceImpl.java
@@ -1,17 +1,25 @@
 package com.smunity.api.service.impl;
 
 import com.smunity.api.common.CommonResponse;
-import com.smunity.api.data.dto.SignInResultDto;
 import com.smunity.api.data.dto.SignUpResultDto;
 import com.smunity.api.data.entity.User;
 import com.smunity.api.data.repository.UserRepository;
 import com.smunity.api.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 
+@Service
 public class AccountServiceImpl implements AccountService {
     public UserRepository userRepository;
     public PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AccountServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public SignUpResultDto signUp(String username, String password, String email, boolean is_admin) {
