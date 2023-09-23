4aa4ba04ef8686c69c06e9c5cde00631bd85e3b8
hyunmin0317 <choihm9903@naver.com>
2023-09-23 13:32:27 +0900

    feat: JwtAuthenticationFilter 작성 (#7)

--- src/main/java/com/smunity/api/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/smunity/api/config/security/JwtAuthenticationFilter.java
@@ -1,16 +1,14 @@
 package com.smunity.api.config.security;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
