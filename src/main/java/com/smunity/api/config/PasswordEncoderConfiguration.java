e79e63fa1d5927603983a695fcd08ae7c9a5807b
hyunmin0317 <choihm9903@naver.com>
2023-09-23 16:36:20 +0900

    feat: Pbkdf2PasswordEncoder 적용 (#7)

--- src/main/java/com/smunity/api/config/PasswordEncoderConfiguration.java
+++ src/main/java/com/smunity/api/config/PasswordEncoderConfiguration.java
@@ -1,16 +1,15 @@
 package com.smunity.api.config;
 
+import com.smunity.api.config.security.encoder.Pbkdf2PasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 public class PasswordEncoderConfiguration {
-
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new Pbkdf2PasswordEncoder();
     }
-
 }
