6e5ef1520b225eba054ecf83cac0fad4263a44ce
hyunmin0317 <choihm9903@naver.com>
2023-09-24 16:23:44 +0900

    refactor: 청원 dto 리팩토링 (#23)

--- src/main/java/com/smunity/api/domain/petition/dto/PetitionResponseDto.java
+++ src/main/java/com/smunity/api/domain/petition/dto/PetitionResponseDto.java
@@ -12,9 +12,12 @@ import java.time.LocalDateTime;
 public class PetitionResponseDto extends PetitionDto {
     private Long id;
 
+    private Long author_id;
+
     @Builder(builderMethodName = "petitionResponseDtoBuilder")
     public PetitionResponseDto(Long id, String subject, String content, Integer category, boolean anonymous, LocalDateTime create_date, LocalDateTime end_date, LocalDateTime modify_date, Integer status, Long author_id) {
-        super(subject, content, category, anonymous, create_date, end_date, modify_date, status, author_id);
+        super(subject, content, category, anonymous, create_date, end_date, modify_date, status);
         this.id = id;
+        this.author_id = author_id;
     }
 }
