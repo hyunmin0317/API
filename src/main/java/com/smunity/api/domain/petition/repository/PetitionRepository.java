c4d277e1e18989b8a5633bd9e170319dbb321543
hyunmin0317 <choihm9903@naver.com>
2023-09-24 14:38:47 +0900

    feat: Petition Repository 작성 (#20)

---
+++ src/main/java/com/smunity/api/domain/petition/repository/PetitionRepository.java
@@ -0,0 +1,7 @@
+package com.smunity.api.domain.petition.repository;
+
+import com.smunity.api.domain.petition.domain.Petition;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetitionRepository extends JpaRepository<Petition, Long> {
+}
