5cb090f19ea7097685cf9653eb66619a239b1b20
hyunmin0317 <choihm9903@naver.com>
2023-09-27 17:07:32 +0900

    feat: DepartmentRepository 작성 (#36)

--- src/main/java/com/smunity/api/domain/account/repository/DepartmentRepository.java
+++ src/main/java/com/smunity/api/domain/account/repository/DepartmentRepository.java
@@ -1,7 +1,9 @@
 package com.smunity.api.domain.account.repository;
 
+import com.smunity.api.domain.account.domain.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface DepartmentRepository extends JpaRepository<DepartmentRepository, Long> {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Department getByName(String name);
 }
