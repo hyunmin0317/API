53c5fee182c9f21bff92be56c2e8165546c7bf2a
hyunmin0317 <choihm9903@naver.com>
2023-09-27 16:48:06 +0900

    feat: Department 레포지토리 작성 (#36)

---
+++ src/main/java/com/smunity/api/domain/account/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.smunity.api.domain.account.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface DepartmentRepository extends JpaRepository<DepartmentRepository, Long> {
+}
