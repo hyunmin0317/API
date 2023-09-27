3d4fb2c4740335eb87ffab624315d6bcd777daf5
hyunmin0317 <choihm9903@naver.com>
2023-09-27 17:12:32 +0900

    feat: Year 엔티티 작성 (#36)

---
+++ src/main/java/com/smunity/api/domain/account/domain/Year.java
@@ -0,0 +1,32 @@
+package com.smunity.api.domain.account.domain;
+
+import lombok.Data;
+import javax.persistence.*;
+
+
+@Entity
+@Data
+@Table(name = "accounts_year")
+public class Year {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String year;
+
+    @Column
+    private int major_i;
+
+    @Column
+    private int major_s;
+
+    @Column
+    private int culture;
+
+    @Column
+    private int culture_cnt;
+
+    @Column
+    private int all;
+}
