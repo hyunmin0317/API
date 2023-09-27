2c18bb3ce4a1c3faae62e742009337beb2a4a632
hyunmin0317 <choihm9903@naver.com>
2023-09-27 16:47:07 +0900

    feat: Department 엔티티 작성 (#36)

---
+++ src/main/java/com/smunity/api/domain/account/domain/Department.java
@@ -0,0 +1,27 @@
+package com.smunity.api.domain.account.domain;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "accounts_department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String college;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column
+    private String url;
+}
