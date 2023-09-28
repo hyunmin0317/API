8755389e3ee85eebabd78f82d76570727f922c25
hyunmin0317 <choihm9903@naver.com>
2023-09-28 12:11:23 +0900

    feat: Answer 엔티티 작성 (#48)

---
+++ src/main/java/com/smunity/api/domain/qna/domain/Answer.java
@@ -0,0 +1,48 @@
+package com.smunity.api.domain.qna.domain;
+
+import com.smunity.api.domain.account.domain.User;
+import com.smunity.api.global.common.BaseEntity;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "qna_answer")
+public class Answer extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Boolean anonymous;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    @ToString.Exclude
+    private User author;
+
+    @OneToOne
+    @JoinColumn(name = "question_id")
+    @ToString.Exclude
+    private Question question;
+
+    @Builder
+    public Answer(LocalDateTime create_date, LocalDateTime modify_date, Long id, String content, Boolean anonymous, User author, Question question) {
+        super(create_date, modify_date);
+        this.id = id;
+        this.content = content;
+        this.anonymous = anonymous;
+        this.author = author;
+        this.question = question;
+    }
+}
