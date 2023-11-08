6682f35ea86f85e9938ab14c67661d7db4c704d1
hyunmin0317 <choihm9903@naver.com>
2023-11-08 21:16:20 +0900

    feat: enum Status 추가 (#118)

---
+++ src/main/java/com/smunity/api/domain/petition/entity/Status.java
@@ -0,0 +1,17 @@
+package com.smunity.api.domain.petition.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public enum Status {
+    IN_PROGRESS("진행중"),
+    AWAITING_RESPONSE("답변대기"),
+    RESPONSE_COMPLETED("답변완료"),
+    EXPIRED("만료"),
+    REJECTED("반려");
+
+    private final String code;
+}
