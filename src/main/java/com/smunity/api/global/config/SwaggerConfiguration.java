1a5962ce838f4880d1eafd9f36f6db28a4556a2c
hyunmin0317 <choihm9903@naver.com>
2023-11-08 18:30:11 +0900

    feat: 페이지네이션 swagger 설정 (#109)

--- src/main/java/com/smunity/api/global/config/SwaggerConfiguration.java
+++ src/main/java/com/smunity/api/global/config/SwaggerConfiguration.java
@@ -1,10 +1,15 @@
 package com.smunity.api.global.config;
 
+import com.fasterxml.classmate.TypeResolver;
+import com.smunity.api.global.config.swagger.MyPagable;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -13,10 +18,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@RequiredArgsConstructor
 public class SwaggerConfiguration {
+    private final TypeResolver typeResolver;
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+            .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(Pageable.class),
+                    typeResolver.resolve((MyPagable.class))))
             .apiInfo(apiInfo())
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.smunity.api"))
