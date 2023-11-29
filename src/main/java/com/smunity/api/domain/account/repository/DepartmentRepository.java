2ee63500a1469a593ab49f72bef33bb3fb2195c2
hyunmin0317 <choihm9903@naver.com>
2023-11-29 21:45:06 +0900

    refactor: account repository 리팩토링 (#132)

--- src/main/java/com/smunity/api/domain/account/repository/DepartmentRepository.java
+++ src/main/java/com/smunity/api/domain/account/repository/DepartmentRepository.java
@@ -2,8 +2,8 @@ package com.smunity.api.domain.account.repository;
 
 import com.smunity.api.domain.account.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
+import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Optional<Department> findByName(String name);
--- src/main/java/com/smunity/api/domain/account/repository/ProfileRepository.java
+++ src/main/java/com/smunity/api/domain/account/repository/ProfileRepository.java
@@ -3,6 +3,5 @@ package com.smunity.api.domain.account.repository;
 import com.smunity.api.domain.account.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 }
