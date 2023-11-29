992da750caee74e4d59b20159f23c5d83552092e
hyunmin0317 <choihm9903@naver.com>
2023-11-29 21:45:16 +0900

    refactor: account service 리팩토링 (#132)

--- src/main/java/com/smunity/api/domain/account/service/AuthService.java
+++ src/main/java/com/smunity/api/domain/account/service/AuthService.java
@@ -2,10 +2,9 @@ package com.smunity.api.domain.account.service;
 
 import com.smunity.api.domain.account.dto.AuthDto;
 
-import java.io.IOException;
 import java.util.Map;
 
 public interface AuthService {
-    Map<String, String> signIn(AuthDto.Request request) throws IOException;
-    AuthDto.Response getInformation(Map<String, String> cookies) throws IOException;
+    Map<String, String> signIn(AuthDto.Request request);
+    AuthDto.Response getInformation(Map<String, String> cookies);
 }
