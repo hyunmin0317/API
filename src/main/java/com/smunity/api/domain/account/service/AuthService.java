1ba245262aa8a573a05d8a488580535c007dc46c
hyunmin0317 <choihm9903@naver.com>
2023-11-29 21:03:19 +0900

    refactor: 회원인증 Dto 리팩토링 (#132)

--- src/main/java/com/smunity/api/domain/account/service/AuthService.java
+++ src/main/java/com/smunity/api/domain/account/service/AuthService.java
@@ -1,12 +1,11 @@
 package com.smunity.api.domain.account.service;
 
 import com.smunity.api.domain.account.dto.AuthDto;
-import com.smunity.api.domain.account.dto.UserDto;
 
 import java.io.IOException;
 import java.util.Map;
 
 public interface AuthService {
-    Map<String, String> signIn(UserDto.SignIn signInDto) throws IOException;
-    AuthDto getInformation(Map<String, String> cookies) throws IOException;
+    Map<String, String> signIn(AuthDto.Request request) throws IOException;
+    AuthDto.Response getInformation(Map<String, String> cookies) throws IOException;
 }
--- src/main/java/com/smunity/api/domain/account/service/impl/AuthServiceImpl.java
+++ src/main/java/com/smunity/api/domain/account/service/impl/AuthServiceImpl.java
@@ -1,7 +1,6 @@
 package com.smunity.api.domain.account.service.impl;
 
 import com.smunity.api.domain.account.dto.AuthDto;
-import com.smunity.api.domain.account.dto.UserDto;
 import com.smunity.api.domain.account.service.AuthService;
 import com.smunity.api.global.error.exception.RestException;
 import org.jsoup.Connection;
@@ -12,13 +11,12 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.util.Map;
 
-
 @Service
 public class AuthServiceImpl implements AuthService {
     @Override
-    public Map<String, String> signIn(UserDto.SignIn signInDto) throws IOException {
+    public Map<String, String> signIn(AuthDto.Request request) throws IOException {
         Connection.Response res = Jsoup.connect("https://ecampus.smu.ac.kr/login/index.php")
-                .data("username", signInDto.getUsername(), "password", signInDto.getPassword())
+                .data("username", request.getUsername(), "password", request.getPassword())
                 .method(Connection.Method.POST)
                 .execute();
         if (!res.url().toString().equals("https://ecampus.smu.ac.kr/"))
@@ -27,14 +25,14 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
-    public AuthDto getInformation(Map<String, String> cookies) throws IOException {
+    public AuthDto.Response getInformation(Map<String, String> cookies) throws IOException {
         if (cookies == null)
             throw new RestException(HttpStatus.UNAUTHORIZED);
         Document doc = Jsoup.connect("https://ecampus.smu.ac.kr/user/user_edit.php")
                 .cookies(cookies)
                 .method(Connection.Method.GET)
                 .get();
-        return AuthDto.of(getById(doc, "id_firstname"), changeName(getById(doc, "id_department")), getById(doc, "id_email"));
+        return AuthDto.Response.of(getById(doc, "id_firstname"), changeName(getById(doc, "id_department")), getById(doc, "id_email"));
     }
 
     public String getById(Document doc, String id) {
