50efde7cafee1d717d9b68fe6b2c8e54a2dfa6cc
hyunmin0317 <choihm9903@naver.com>
2023-11-30 20:32:45 +0900

    refactor: question repository 리팩토링 (#135)

--- src/main/java/com/smunity/api/domain/question/repository/QuestionRepository.java
+++ src/main/java/com/smunity/api/domain/question/repository/QuestionRepository.java
@@ -3,6 +3,5 @@ package com.smunity.api.domain.question.repository;
 import com.smunity.api.domain.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
