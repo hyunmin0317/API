4f60205bb977eb0061ae18a2b1a5ea76f5298564
hyunmin0317 <choihm9903@naver.com>
2023-11-30 20:29:49 +0900

    refactor: petition controller 리팩토링 (#134)

--- src/main/java/com/smunity/api/domain/petition/controller/AgreementController.java
+++ src/main/java/com/smunity/api/domain/petition/controller/AgreementController.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
